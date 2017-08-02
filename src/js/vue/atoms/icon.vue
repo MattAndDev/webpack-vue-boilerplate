9fade145cb0dd3ba11cdbc52b757c954ae919994
Matteo Maronati <me@mattinthe.net>
2017-08-02 16:54:38 +0200

    Rw: refactorung vue structure to match atomic design

---
+++ src/js/vue/atoms/icon.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg class="i-logo-mark funnelHeader_logo">
+    <use :xlink:href="unicorn.url"></use>
+  </svg>
+</template>
+<script>
+
+import unicorn from 'icons/unicorn.svg'
+
+export default {
+  name: 'icon',
+  data () {
+    return {
+      iconName: String,
+      className: String
+    }
+  },
+}
+</script>
---
+++ src/js/vue/views/home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div ref='home' class="home">
+    <div class="u-cw">
+      <h1>home</h1>
+      <svg class="i-logo-mark funnelHeader_logo">
+        <use :xlink:href="unicorn.url"></use>
+      </svg>
+    </div>
+  </div>
+</template>
+<script>
+
+import Icon from 'atoms/icon.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Icon
+  },
+  data () {
+    return {
+      unicorn: unicorn
+    }
+  },
+}
+</script>

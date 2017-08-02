356531bfd3b639f157971f14b028938dee8299cd
Matteo Maronati <me@mattinthe.net>
2017-08-02 18:37:21 +0200

    Up: intiial icon atom

--- src/js/vue/atoms/icon.vue
+++ src/js/vue/atoms/icon.vue
@@ -1,19 +1,23 @@
 <template>
-  <svg class="i-logo-mark funnelHeader_logo">
-    <use :xlink:href="unicorn.url"></use>
+  <svg class="i-funnel-close funnelHeader_close">
+    <use :xlink:href="this.icon.url"></use>
   </svg>
 </template>
 <script>
 
-import unicorn from 'icons/unicorn.svg'
 
 export default {
   name: 'icon',
   data () {
     return {
-      iconName: String,
-      className: String
+      icon: Object
     }
   },
+  props: [
+    'iconName'
+  ],
+  mounted () {
+    this.icon = require(`icons/${this.iconName}.svg`).default
+  }
 }
 </script>
